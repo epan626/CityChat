272cd05eac656b1ec82f44591191070f244cd67a
cpaisan <cppaisan@gmail.com>
2017-02-27 14:47:11 -0800

    first draft storyboard completed

---
+++ chat/DirectMessageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DirectMessageViewController.swift
+//  chat
+//
+//  Created by Charles Paisan on 2/27/17.
+//  Copyright © 2017 Eric Pan. All rights reserved.
+//
+
+import UIKit
+
+class DirectMessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
