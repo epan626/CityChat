e64bcf3febce04e9dd968727ac334147cbf13e63
cpaisan <cppaisan@gmail.com>
2017-02-27 18:10:34 -0800

    fixed DM view to show properly and added responsive keyboard to DM view

--- chat/UserListViewController.swift
+++ chat/UserListViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class UserListViewController: UIViewController{
+class UserListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var userListTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = userListTableView.dequeueReusableCell(withIdentifier: "userCell")
+        cell?.backgroundColor = UIColor.blue
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let directMessageController = self.storyboard?.instantiateViewController(withIdentifier: "DirectMessageController")
+        navigationController?.pushViewController(directMessageController!, animated: true)
     }
 
 }
